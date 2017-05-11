0d9a4204993fec6e486bf72fe08d769fba71c7b3
JST5000 <aim4math@gmail.com>
2017-05-11 14:38:09 -0700

    Added placeholder art and added basic implementation of the gameboard's rigid states.
    Basic gameboard added with some placeholder art.

---
+++ Cow Shooter/Assets/Delete_On_Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete_On_Contact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Should delete all objects that collide with it.
+        ContactFilter2D all = new ContactFilter2D();
+        all.NoFilter();
+        Collider2D[] overlapping = null;
+        int size = gameObject.GetComponent<Collider2D>().OverlapCollider(all, overlapping);
+        for (int i = 0; i < size; i++)
+        {
+             Destroy(overlapping[i].gameObject);   
+        }
+    }
+}
