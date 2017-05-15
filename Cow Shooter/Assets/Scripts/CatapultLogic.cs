a8f508758fec5a4c39f9acf039433ad6f5aca2fa
JST5000 <aim4math@gmail.com>
2017-05-15 11:43:14 -0700

    Started making the catapult and throwable scripts
    Created a rough template for what functions will need to be implemented
    in both scripts. Tetris-single is the current testing object for
    throwables.

---
+++ Cow Shooter/Assets/Scripts/CatapultLogic.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultLogic : MonoBehaviour {
+
+    private bool loaded;
+    private bool fire;
+    private float power;
+    private int maxPower;
+    private int milliUntilMaxPower;
+    private bool chargingUp;
+
+	// Use this for initialization
+	void Start () {
+        loaded = false;
+        fire = false;
+        power = 0;
+        maxPower = 100;
+        milliUntilMaxPower = 1000;
+        chargingUp = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!loaded)
+        {
+            
+        }
+        if(fire)
+        {
+            launchThrowable();
+        }
+        if(chargingUp)
+        {
+            increasePower();
+        }
+	}
+
+    //TODO
+    private void Load()
+    {
+        //Get random throwable from pool
+        //GameObject throwable = 
+        //Attach(throwable);
+        //loaded = true;
+    }
+
+    public void Attach(GameObject throwable)
+    {
+        FixedJoint2D connection = GetComponent<FixedJoint2D>();
+        connection.connectedBody = throwable.GetComponent<Rigidbody2D>();
+
+    }
+
+    //TODO
+    private void launchThrowable()
+    {
+
+    }
+
+    private void increasePower()
+    {
+
+        float deltaPower = Time.deltaTime * 1000 / milliUntilMaxPower * maxPower;
+        if(power + deltaPower > maxPower)
+        {
+            power = maxPower;
+        } else
+        {
+            power = power + deltaPower;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        chargingUp = true;
+        power = 0;
+    }
+
+    private void OnMouseUp()
+    {
+        chargingUp = false;
+        if (loaded)
+        {
+            fire = true;
+        }
+    }
+
+
+
+
+
+    
+}
---
+++ Cow Shooter/Assets/Scripts/Throwable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour {
+
+    private bool isFirstCollision;
+    public bool isAttached;
+
+	// Use this for initialization
+	void Start () {
+        isFirstCollision = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
