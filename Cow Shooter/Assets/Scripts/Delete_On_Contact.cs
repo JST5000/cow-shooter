22e5dcf7a3d363ea9d8e49ae6a250fe8c95fb80b
JST5000 <aim4math@gmail.com>
2017-05-15 19:43:37 -0700

    Messed around with the catapults

--- Cow Shooter/Assets/Scripts/Delete_On_Contact.cs
+++ Cow Shooter/Assets/Scripts/Delete_On_Contact.cs
@@ -11,14 +11,10 @@ public class Delete_On_Contact : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Should delete all objects that collide with it.
-        ContactFilter2D all = new ContactFilter2D();
-        all.NoFilter();
-        Collider2D[] overlapping = null;
-        int size = gameObject.GetComponent<Collider2D>().OverlapCollider(all, overlapping);
-        for (int i = 0; i < size; i++)
-        {
-             Destroy(overlapping[i].gameObject);   
-        }
+      
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
     }
 }
---
+++ Cow Shooter/Assets/Scripts/Launch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+    private Transform rotator;
+    private bool positivity;
+    public Vector3 vel;
+    public float max_rot;
+    public float min_rot;
+    public bool counterclockwise;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rotator = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positivity = vel.z < 0;
+        if(counterclockwise)
+        {
+            if ((positivity && rotator.rotation.eulerAngles.z > max_rot) || (!positivity && rotator.rotation.eulerAngles.z < min_rot))
+            {
+                vel = new Vector3(0, 0, -vel.z);
+            }
+        } 
+        rotator.Rotate(vel);
+    }
+}
