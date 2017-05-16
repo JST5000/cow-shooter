ef7bb2c0c9db2fc3891295d0c104ef12d960d182
JST5000 <aim4math@gmail.com>
2017-05-15 22:55:48 -0700

    Trying to make collisions happen?

--- Cow Shooter/Assets/Scripts/Launch.cs
+++ Cow Shooter/Assets/Scripts/Launch.cs
@@ -67,7 +67,6 @@ public class Launch : MonoBehaviour {
             isIdle = true;
             direction = -direction;
         }
-        print(current_rot);
         rotator.Rotate(direction * vel);
         
     }
