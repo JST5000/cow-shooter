15aed1fc1ce271beeee63596a67b2ab0f2d8ebde
JST5000 <aim4math@gmail.com>
2017-05-15 21:50:45 -0700

    Catapults now have "launch" code on RMB and LMB for the respective sides.

--- Cow Shooter/Assets/Scripts/Launch.cs
+++ Cow Shooter/Assets/Scripts/Launch.cs
@@ -5,31 +5,77 @@ using UnityEngine;
 public class Launch : MonoBehaviour {
 
     private Transform rotator;
-    private bool positivity;
-    public Vector3 vel;
+    private Vector3 vel;
     public float max_rot;
+    private float current_rot;
     public float min_rot;
-    public bool counterclockwise;
+    public bool isLeftFacing;
+    private int direction;
+    private bool isIdle;
 
 
     // Use this for initialization
     void Start()
     {
+        current_rot = 0;
         rotator = GetComponent<Transform>();
-
+        if (isLeftFacing)
+        {
+            direction = -1;
+        }
+        else
+        {
+            direction = 1;
+        }
+        vel = new Vector3(0, 0, 2);
+        isIdle = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        positivity = vel.z < 0;
-        if(counterclockwise)
+        int input_type = 1; //rmb
+        if(isLeftFacing)
         {
-            if ((positivity && rotator.rotation.eulerAngles.z > max_rot) || (!positivity && rotator.rotation.eulerAngles.z < min_rot))
-            {
-                vel = new Vector3(0, 0, -vel.z);
-            }
-        } 
-        rotator.Rotate(vel);
+            input_type = 0; //lmb
+        }
+        if (Input.GetMouseButtonDown(input_type) && isIdle)
+        {
+            activate();
+        }
+        if (!isIdle)
+        {
+            rotateToMax();
+        }
     }
+
+    private void rotateToMax()
+    {
+        if (isLeftFacing)
+        {
+            current_rot += -1 * direction * vel.z;
+        } else
+        {
+            current_rot += direction * vel.z;
+        }
+        if (current_rot > max_rot)
+        {
+            direction = -direction;
+        }
+        if (current_rot <= min_rot)
+        {
+            isIdle = true;
+            direction = -direction;
+        }
+        print(current_rot);
+        rotator.Rotate(direction * vel);
+        
+    }
+
+    public void activate()
+    {
+        isIdle = false;
+    }
+
+
 }
