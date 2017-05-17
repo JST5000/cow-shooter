c50187bf6aef8032b9f2db78f208235fcfe152a7
AJ.P <parkeraj256@gmail.com>
2017-05-17 15:48:15 -0700

    background image is finished

---
+++ Cow Shooter/Assets/Scripts/BackgroundTransparency.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTransparency : MonoBehaviour {
+    public double transparancy;
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer spriteRenderer=GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(1f, 1f, 1f, (float)transparancy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
