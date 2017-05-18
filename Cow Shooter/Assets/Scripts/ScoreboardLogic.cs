daded25a03d40795d3e11f8567cf11e78da7216e
AJ.P <parkeraj256@gmail.com>
2017-05-17 20:46:46 -0700

    forgot one

---
+++ Cow Shooter/Assets/Scripts/ScoreboardLogic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardLogic : MonoBehaviour {
+    GameObject holder;
+    public float updateDelay;
+    private float updateCount;
+    public int blueFinalScore;
+    public int redFinalScore;
+    public int boardSize;
+    // Use this for initialization
+    void Start () {
+        GameObject tempObject = GameObject.Find("BackgroundTarget");
+        PointCalculator calculator = tempObject.GetComponent<PointCalculator>();
+        boardSize = calculator.size;
+        blueFinalScore = 0;
+        redFinalScore = 0;
+        holder =GameObject.Find("ThrowableInstanceHolder");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateCount += Time.deltaTime;
+        if (updateCount > updateDelay)
+        {
+            updateScore();
+            updateCount = 0;
+        }
+	}
+    void updateScore()
+    {
+        blueFinalScore = 0;
+        redFinalScore = 0;
+        InsideChecker[] pointTotals = holder.GetComponentsInChildren<InsideChecker>();
+        foreach(InsideChecker checker in pointTotals)
+        {
+            if (checker.team == Team.suggestedTeams.Blue)
+            {
+                blueFinalScore += checker.score;
+            }
+            else
+            {
+                redFinalScore += checker.score;
+            }
+    
+        }
+        print("BlueScore="+ ((double)blueFinalScore /boardSize)*100+" RedScore="+(((double)redFinalScore)/boardSize)*100);
+        
+    }
+}
