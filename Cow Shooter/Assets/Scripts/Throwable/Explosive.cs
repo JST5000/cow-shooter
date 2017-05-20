72b6cd007f72080391535b58da719a7449e47591
AJ.P <parkeraj256@gmail.com>
2017-05-19 20:43:54 -0700

    Bomb added to game

---
+++ Cow Shooter/Assets/Scripts/Throwable/Explosive.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour {
+    float explosion_delay;
+    public float explosion_rate = 1f;
+    public float explosion_max_size = 10f;
+    public float explosion_speed = 1f;
+    public float max_delay;
+    public float power;
+    bool exploded = false;
+    float current_radius=0f;
+    public CircleCollider2D explosion_radius;
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<Transform>().parent.tag != "ThrowableHolder")
+        {
+            explosion_delay = max_delay;
+            // explosion_radius = gameObject.GetComponent<CircleCollider2D>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<Transform>().parent.tag != "ThrowableHolder")
+        {
+            explosion_delay -= Time.deltaTime;
+            if (explosion_delay < 0)
+            {
+                exploded = true;
+            }
+        }
+	}
+    private void FixedUpdate()
+    {
+        if (GetComponent<Transform>().parent.tag != "ThrowableHolder")
+        {
+            if (exploded)
+            {
+                if (current_radius < explosion_max_size)
+                {
+                    current_radius += explosion_rate;
+                }
+                else
+                {
+                    Object.Destroy(this.gameObject);
+                }
+
+                explosion_radius.radius = current_radius;
+
+
+
+
+
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (GetComponent<Transform>().parent.tag != "ThrowableHolder")
+        {
+            if (exploded)
+            {
+                print("bomb");
+                if (col.gameObject.GetComponent<Rigidbody2D>() != null)
+                {
+                    Vector2 target = col.gameObject.transform.position;
+                    Vector2 bomb = gameObject.transform.position;
+                    Vector2 direction = power* (target - bomb);
+                    col.gameObject.GetComponent<Rigidbody2D>().AddForce(direction);
+
+                }
+
+
+
+            }
+        }
+    }
+}
