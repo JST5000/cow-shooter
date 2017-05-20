c85f7e8d41487a027fbb6651b247889af007323d
AJ.P <parkeraj256@gmail.com>
2017-05-19 20:54:58 -0700

    fixed merge with master

--- Cow Shooter/Assets/Scripts/Goal_Shape/InsideChecker.cs
+++ Cow Shooter/Assets/Scripts/Goal_Shape/InsideChecker.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InsideChecker : MonoBehaviour {
-    private PolygonCollider2D collider;
+    private Collider2D collider;
     private ArrayList comparePoints;
     public int team;
     public int score;
     // Use this for initialization
     void Start () {
         score = 0;
-        collider= GetComponent<PolygonCollider2D>();
+        if (GetComponent<CircleCollider2D>() != null)
+        {
+            collider = GetComponent<CircleCollider2D>();
+        }
+        else
+        {
+            collider = GetComponent<PolygonCollider2D>();
+        }
         team= GetComponent<Team>().team;
         GameObject allTargets= GameObject.Find("Targets");
         PointCalculator rightTarget = allTargets.GetComponent<PointCalculator>();
