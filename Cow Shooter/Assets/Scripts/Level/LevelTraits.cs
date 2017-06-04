ffeda453f60e3d46d76e2e6d43c311ddaf327b20
JST5000 <aim4math@gmail.com>
2017-06-03 18:54:02 -0700

    added a temp tv

---
+++ Cow Shooter/Assets/Scripts/Level/LevelTraits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTraits : MonoBehaviour {
+
+	/* Brainstorm for what levels should contain
+	 * Background Image
+	 * Target (Or list of possible targets)
+	 * AI script
+	 * "Character" for enemy 
+	 * AI Throwable List 
+	 */
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Cow Shooter/Assets/Scripts/Throwable/FlatScreenFirstCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlatScreenFirstCollision : FirstCollision {
+
+	public Sprite broken;
+
+	public override void onFirstCollision () {
+		collidedAfterLaunch = true;
+		GetComponent<SpriteRenderer> ().sprite = broken;
+		GetComponent<SpriteRenderer> ().sortingLayerName = "Throwables";
+	}
+}
