d7e84a4634d726be0188a0a718ac77940228a085
JST5000 <aim4math@gmail.com>
2017-06-14 23:34:16 -0700

    Added Deck Library for storage of deck lists

---
+++ Cow Shooter/Assets/Scripts/Decks/DeckLibrary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckLibrary : MonoBehaviour {
+
+	public static void giveAdminDeckTo(PlayerData deckHolder) {
+		deckHolder.throwableNames.Clear ();
+		deckHolder.throwableNames.Add ("Anchor");
+		deckHolder.throwableNames.Add ("Bomb");
+		deckHolder.throwableNames.Add ("BouncyHouse");
+		deckHolder.throwableNames.Add ("Chair");
+		deckHolder.throwableNames.Add ("Cow");
+		deckHolder.throwableNames.Add ("Doughnut");
+		deckHolder.throwableNames.Add ("FlatscreenRed");
+		deckHolder.throwableNames.Add ("Long-Blue");
+		deckHolder.throwableNames.Add ("Pan");
+		deckHolder.throwableNames.Add ("Piano");
+		deckHolder.throwableNames.Add ("Present_Red");
+		deckHolder.throwableNames.Add ("Square-Blue");
+		deckHolder.throwableNames.Add ("Teddy");
+		deckHolder.throwableNames.Add ("Tetris_T");
+	}
+
+	public static void giveBomberManDeckTo(PlayerData deckHolder) {
+		deckHolder.throwableNames.Clear ();
+		deckHolder.throwableNames.Add ("Bomb");
+		deckHolder.throwableNames.Add ("Present_Red");
+	}
+}
