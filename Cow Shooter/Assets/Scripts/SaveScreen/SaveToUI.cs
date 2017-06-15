935eb0fd0d874781aaf74cefc894c342288641e5
JST5000 <aim4math@gmail.com>
2017-06-14 20:38:29 -0700

    Added selection box to saves

--- Cow Shooter/Assets/Scripts/SaveScreen/SaveToUI.cs
+++ Cow Shooter/Assets/Scripts/SaveScreen/SaveToUI.cs
@@ -6,6 +6,18 @@ using UnityEngine.UI;
 public class SaveToUI : MonoBehaviour {
 
 	private PlayerAccount save;
+	private bool hidden = true;
+
+	void Update() {
+		if (hidden && SaveSlots.currentSaveSlots.chosenSave != null &&
+				SaveSlots.currentSaveSlots.chosenSave.accountInfo.username == save.accountInfo.username) {
+			showSelectionBox ();
+			hidden = false;
+		} else if(!hidden && SaveSlots.currentSaveSlots.chosenSave != null && SaveSlots.currentSaveSlots.chosenSave.accountInfo.username != save.accountInfo.username) {
+			hideSelectionBox ();
+			hidden = true;
+		}
+	}
 
 	public void adoptSave(PlayerAccount newSave) {
 		save = newSave;
@@ -28,5 +40,13 @@ public class SaveToUI : MonoBehaviour {
 		} else {
 			print ("Tried to set save, but there was no save attached to this SaveToUI");
 		}
+	}
+
+	private void showSelectionBox() {
+		GetComponentInChildren<SelectionLogic> ().turnSelectionBoxOn ();
+	}
+
+	private void hideSelectionBox() {
+		GetComponentInChildren<SelectionLogic> ().turnSelectionBoxOff ();
 	}
 }
---
+++ Cow Shooter/Assets/Scripts/SaveScreen/SelectionLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		turnSelectionBoxOff ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void turnSelectionBoxOn() {
+		GetComponent<Image> ().color = new Color (GetComponent<Image> ().color.r, 
+			GetComponent<Image> ().color.g, GetComponent<Image> ().color.b, 1);
+	}
+
+	public void turnSelectionBoxOff() {
+		GetComponent<Image> ().color = new Color (GetComponent<Image> ().color.r, 
+			GetComponent<Image> ().color.g, GetComponent<Image> ().color.b, 0);
+	}
+}
