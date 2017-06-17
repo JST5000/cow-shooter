e8a987dc146fbf128d788e529b5398306c073c9a
JST5000 <aim4math@gmail.com>
2017-06-16 21:57:00 -0700

    Random stuff

---
+++ Cow Shooter/Assets/Scripts/Player/CampaignHistory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using 
+
+
+public class CampaignHistory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
